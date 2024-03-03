b1cd01c9511c32b4057361a8baaf7115d4ea3456
Vismay Ravikumar <vismayravikumar@gmail.com>
2024-03-03 13:34:59 -0500

    Merge #2

--- client/src/sports_bet_util.js
+++ client/src/sports_bet_util.js
@@ -1,20 +1,18 @@
 export function getRandomGame() {
   const year = 2023 // Math.floor(Math.random() * 7 + 2015);
-  const url = `https://v1.basketball.api-sports.io/odds?league=12&season=${year + "-" + (year+1)}`
+  const url = `https://v1.basketball.api-sports.io/odds?league=12&season=${year + "-" + (year + 1)}`
   return fetch(url, {
     method: "GET",
     headers: {
-      "x-rapidapi-key": "8097ad8b9eb665d99d8e442839cce0cc"
+      "x-rapidapi-key": "9f18683bd23d36897c490739f4780e16"
     }
   })
     .then((response) => response.json())
     .then((json) => {
       const response = json["response"];
-      // console.log(response);
       const random = Math.floor(Math.random() * response.length);
       const game = response[random]["game"];
-      // console.log(game["teams"]["home"]["name"] + " vs " + game["teams"]["away"]["name"]);
-      return game["id"]
+      return game["id"];
     })
     .catch((error) => {
       throw `GET request to "${url}" failed with error:\n${error}`
@@ -33,7 +31,9 @@ export function getBets(gameId) {
     .then((json) => {
       console.log(json);
       const bookmakers = json["response"][0]["bookmakers"];
-      for (let i = 0; i < bookmakers.length; i ++) {
+
+      for (let i = 0; i < bookmakers.length; i++) {
+
         if (bookmakers[i]["name"] === "Marathon Bet") {
           console.log("Found Marathon bet")
           const bets = bookmakers[i]["bets"];
@@ -42,14 +42,17 @@ export function getBets(gameId) {
             "Highest Scoring Quarter"
           ]
           let targetBets = {}
-          for (let j = 0; j < bets.length; j ++) {
+
+          for (let j = 0; j < bets.length; j++) {
             if (targetBetNames.includes(bets[j]["name"])) {
               targetBets[bets[j]["name"]] = bets[j];
             }
           }
-          return targetBets
+
+          return targetBets;
         }
       }
-      return {}
+
+      return {};
     })
 }
