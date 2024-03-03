1cf657ad312a388e501e91fcdc5d7e43861e55e1
Arjun <arjunkchavan2@gmail.com>
2024-03-03 10:08:18 -0500

    Create disp.js

---
+++ client/src/components/disp.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+const Team = (props) => {
+    let logo = require('props.logo_path')
+
+    return (
+        <div className = "Team">
+            <h5>props.name</h5>
+            <img src={Image.default} alt = "image not found :("/>
+
+
+
+        </div>
+
+
+    )
+}
+
+export default Team
