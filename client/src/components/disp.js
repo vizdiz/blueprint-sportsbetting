7ac38e4a401b082f9c37a37e924a737e5ea64837
Arjun <arjunkchavan2@gmail.com>
2024-03-03 10:49:24 -0500

    Team component push #1

--- client/src/components/disp.js
+++ client/src/components/disp.js
@@ -1,15 +1,42 @@
 import React, {useState} from 'react';
 
 const Team = (props) => {
-    let logo = require('props.logo_path')
-
+    let img = 'https://media.api-sports.io/basketball/teams/'+props.num+'.png'
+    const teams = [
+        "Atlanta Hawks",
+        "Boston Celtics",
+        "Brooklyn Nets",
+        "Charlotte Hornets",
+        "Chicago Bulls",
+        "Cleveland Cavaliers",
+        "Dallas Mavericks",
+        "Denver Nuggets",
+        "Detroit Pistons",
+        "Golden State Warriors",
+        "Houston Rockets",
+        "Indiana Pacers",
+        "Los Angeles Clippers",
+        "Los Angeles Lakers",
+        "Memphis Grizzlies",
+        "Miami Heat",
+        "Milwaukee Bucks",
+        "Minnesota Timberwolves",
+        "New Orleans Pelicans",
+        "New York Knicks",
+        "Oklahoma City Thunder",
+        "Orlando Magic",
+        "Philadelphia 76ers",
+        "Phoenix Suns",
+        "Portland Trail Blazers",
+        "Sacramento Kings",
+        "San Antonio Spurs",
+        "Toronto Raptors",
+        "Utah Jazz",
+        "Washington Wizards"]
     return (
         <div className = "Team">
-            <h5>props.name</h5>
-            <img src={Image.default} alt = "image not found :("/>
-
-
-
+            <h5>teams[props.num - 132]</h5>
+            <img src={img.default} alt="image not found"/>
         </div>
 
 
